275a6693437bbde146331011d4cbc4f053229e2e
i4can <i4can9@gmail.com>
2023-01-06 15:10:57 +0800

    项目初始结构创建

---
+++ mymall-coupon/src/test/java/com/xlg/mymall/coupon/MymallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlg.mymall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
